b40e6c3728da1fe4e16b94e038a627d4b1965c47
stephane <stephane@jumpto.ch>
2025-04-05 20:23:02 +0200

    fix: import camt partner

--- family_expense/models/account_bank_statement_line.py
+++ family_expense/models/account_bank_statement_line.py
@@ -22,8 +22,7 @@ class AccountBankStatementLine(models.Model):
 
             if 'partner_name' in val.keys() and 'partner_id' not in val.keys():
                 for partner in self.env['res.partner'].search([]):
-                    # log statement_name in debug
-                    _logger.warning("partner_name: %s", partner.id)
+                    #_logger.debug("partner id: %s", partner.id)
                     statement_name = partner.statement_name.lower() if partner.statement_name else partner.name.lower()
                     state_line_ref = val['partner_name'].lower()
 
@@ -33,20 +32,13 @@ class AccountBankStatementLine(models.Model):
 
             if 'partner_id' not in val.keys() and 'partner_name' not in val.keys():
                 for partner in self.env['res.partner'].search([]):
-                    _logger.warning("partner_name: %s", partner.id)
+                    #_logger.debug("partner id: %s", partner.id)
                     state_line_ref = val['payment_ref'].lower()
                     statement_name = partner.statement_name.lower() if partner.statement_name else partner.name.lower()
 
                     if isinstance(statement_name, str) and statement_name in state_line_ref:
                         val['partner_id'] = partner.id
                         break
-
-                    #log statement_name in debug
-                    _logger.warning("partner_name: %s", partner.name)
-                    _logger.warning("statement_name: %s", statement_name)
-                    _logger.warning("test: %s", isinstance(statement_name, str) and statement_name in state_line_ref)
-
-
 
             new_vals.append(val)
 
