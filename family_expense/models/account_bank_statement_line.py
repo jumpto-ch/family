4a4cbd4310e64da16304ff1991dd377dd8943e97
stephane <stephane@jumpto.ch>
2025-04-05 08:55:04 +0200

    fix: import camt partner

--- family_expense/models/account_bank_statement_line.py
+++ family_expense/models/account_bank_statement_line.py
@@ -1,7 +1,9 @@
+import logging
 import re
 
 from odoo import models
 
+_logger = logging.getLogger(__name__)
 
 class AccountBankStatementLine(models.Model):
     _inherit = ("account.bank.statement.line",)
@@ -32,9 +34,21 @@ class AccountBankStatementLine(models.Model):
                     state_line_ref = val['payment_ref'].lower()
                     statement_name = partner.statement_name.lower() if partner.statement_name else partner.name.lower()
 
+                    # log statement_name in debug
+                    _logger.warning("partner_name: %s", partner.name)
+                    _logger.warning("statement_name: %s", statement_name)
+                    _logger.warning("test: %s", isinstance(statement_name, str) and statement_name in state_line_ref)
+
                     if isinstance(statement_name, str) and statement_name in state_line_ref:
                         val['partner_id'] = partner.id
                         break
+
+                    #log statement_name in debug
+                    _logger.warning("partner_name: %s", partner.name)
+                    _logger.warning("statement_name: %s", statement_name)
+                    _logger.warning("test: %s", isinstance(statement_name, str) and statement_name in state_line_ref)
+
+
 
             new_vals.append(val)
 
