ed7782873893621ca87f28e34bb1daed6b70470a
joel <joel@jumpto.ch>
2026-01-13 17:09:47 +0100

    Extend finding better partner if camt partner_name giving 'NOTPROVIDED'

--- family_expense/models/account_statement_import_camt_parser.py
+++ family_expense/models/account_statement_import_camt_parser.py
@@ -34,37 +34,52 @@ class AccountStatementImportCamtParser(models.AbstractModel):
             )]
         bank_node = node.xpath("./ns:Acct/ns:Svcr/ns:FinInstnId/ns:BICFI", namespaces={"ns": ns})
         # Pattern could be RmtInf/Ustrd - AddtlTxInf - AddtlNtryInf - Refs/InstrId
-        new_fields_pattern = ["RmtInf/Ustrd"]
+        new_fields_pattern = ["AddtlNtryInf"]
 
         for transaction in result['transactions']:
             if 'partner_name' in transaction.keys() and 'partner_id' not in transaction.keys():
-                # find the correct partner
+                # reset variables
                 filtered_partner = None
-                partner = _find_matching_partner(transaction['partner_name'])
-                if partner:
-                    # if partner and bank match filters
-                    partner_name = partner.statement_name if partner.statement_name else partner.name
-                    if (bank_node and bank_node[0].text in bank_filter and
-                        any(all(any(solution in partner_name for solution in condition)
-                                for condition in partner_filter)
-                            for partner_filter in partner_list_filter)):
-                        # try to find specific field in narration
-                        for new_field_pattern in new_fields_pattern:
-                            match = re.search(rf'\({new_field_pattern}\):([^\n]*)',
-                                              transaction['narration'])
+                partner_in_filter = False
 
-                            # if field exist in narration, try to find matching partner
-                            research_string = match.group(1).strip() if match else ''
-                            filtered_partner = _find_matching_partner(research_string)
-                            # if more specific partner found, stop searching
-                            if filtered_partner:
-                                transaction['partner_id'] = filtered_partner.id
-                                break
-                        # partner in filter but no more specific partner found, remove partner
-                        if not filtered_partner:
-                            transaction['partner_id'] = False
-                    else:
-                        # partner is found but not in filter, use it
+                # FILTER bank_filter
+                bank_in_filter = bank_node and bank_node[0].text in bank_filter
+
+                # search partner in partner_name
+                partner = _find_matching_partner(transaction['partner_name'])
+                # no partner found
+                # we flag it to find a more specific one in narration later
+                no_partner_found = transaction['partner_name'] == 'NOTPROVIDED' or not partner
+
+                if partner:
+                    partner_name = partner.statement_name if partner.statement_name else partner.name
+                    # FILTER partner_list_filter
+                    # partner_list_filter contain the list of partner which need to be more specific
+                    # we flag it to find a more specific one in narration later
+                    partner_in_filter = (
+                        any(all(any(solution in partner_name for solution in condition)
+                                for condition in partner_filter) for partner_filter in partner_list_filter))
+                    if not partner_in_filter:
+                        # a partner is found but is not in filter, use it
                         transaction['partner_id'] = partner.id
+
+                # if we found the target bank and a more specific partner is needed
+                if bank_in_filter and (no_partner_found or partner_in_filter):
+                    # try to find specific field in narration
+                    # FILTER new_fields_pattern
+                    for new_field_pattern in new_fields_pattern:
+                        match = re.search(rf'\({new_field_pattern}\):([^\n]*)',
+                                          transaction['narration'])
+
+                        # if field exist in narration, try to find matching partner
+                        research_string = match.group(1).strip() if match else ''
+                        filtered_partner = _find_matching_partner(research_string)
+                        # if more specific partner found, stop searching
+                        if filtered_partner:
+                            transaction['partner_id'] = filtered_partner.id
+                            break
+                    # partner in filter but no more specific partner found, remove partner
+                    if not filtered_partner:
+                        transaction['partner_id'] = False
 
         return result
